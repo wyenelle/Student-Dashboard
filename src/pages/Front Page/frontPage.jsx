b9dc9b014601a1f7207e6ff84e740f7e7bd8a219
wyenelle <favourkcb@gmail.com>
2001-01-01 01:41:12 +0100

    added font type

--- src/pages/Front Page/frontPage.jsx
+++ src/pages/Front Page/frontPage.jsx
@@ -18,14 +18,14 @@ const FrontPage = () => {
             <h1 className="font-extrabold text-4xl">AltSchool Africa</h1>
         </div>
         <div className="grid grid-cols-2 px-3 md:px-0  md:w-9/12">
-          <article onClick={go_to_blog} className="col-span-2 border border-slate-200  cursor-pointer article shadow shadow-slate-200 shadow-lg md:col-span-1 md:w-10/12 px-4 py-3 h-40 flex items-start justify-center flex-col m-3 rounded-lg  md:mx-auto">
+          <article onClick={go_to_blog} className="col-span-2 border border-slate-200  cursor-pointer article shadow shadow-slate-200 shadow-lg md:col-span-1 md:w-10/12 px-4 py-3 h-40 flex items-start hover:border-blue-500/30 justify-center flex-col m-3 rounded-lg  md:mx-auto">
             <h1 className="font-bold mb-3 text-2xl"> Blog Page</h1>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
               aut sint provident! Dolores, ab voluptates!
             </p>
           </article>
-          <article onClick={go_to_login} className="col-span-2 border border-slate-200 cursor-pointer article shadow shadow-slate-200 shadow-lg md:col-span-1 md:w-10/12 px-4 py-3 h-40  flex items-start justify-center flex-col m-3 rounded-lg md:mx-auto  ">
+          <article onClick={go_to_login} className="col-span-2 border border-slate-200 cursor-pointer article shadow shadow-slate-200 shadow-lg md:col-span-1 md:w-10/12 px-4 py-3 h-40 hover:border-blue-500/30 flex items-start justify-center flex-col m-3 rounded-lg md:mx-auto  ">
             <h1 className="font-bold mb-3 text-2xl"> LMS</h1>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
