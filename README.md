e66e54480288a2954df6eef591f66a296a35bcbb
wyenelle <favourkcb@gmail.com>
2023-03-31 03:48:07 +0100

    updated readme.md

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Student-Dashboard
 
 ## This Proiect is still  work in progress. Far from finished yet
+
+To get the app running:
+
+clone the project
+run 'npm install'
+run 'npm run dev'
+
+
+
+Link to documentation would be out soon
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import {BrowserRouter} from 'react-router-dom'
-
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
         <App />
